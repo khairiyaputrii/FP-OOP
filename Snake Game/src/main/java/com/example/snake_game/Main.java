d4067046cdbb172ed8175d25cf2818d2fdf62574
unknown <khairiya.putri72@gmail.com>
2022-12-07 00:04:34 +0700

    adding direction

--- Snake Game/src/main/java/com/example/snake_game/Main.java
+++ Snake Game/src/main/java/com/example/snake_game/Main.java
@@ -1,6 +1,7 @@
 package com.example.snake_game;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
@@ -20,6 +21,8 @@ public class Main extends Application {
     private Random random;
     private Circle snake;
 
+    private Direction curretDirection;
+
     private void newFood(){
         food = new Circle(random.nextInt(WIDTH),random.nextInt(HEIGHT),RADIUS);
         food.setFill(Color.RED);
@@ -31,31 +34,66 @@ public class Main extends Application {
         root.getChildren().add(snake);
     }
 
-//        @Override
+    private void step(){
+        if (curretDirection == Direction.UP) {
+            snake.setCenterY(snake.getCenterY() - STEP);
+        } else if (curretDirection == Direction.DOWN) {
+            snake.setCenterY(snake.getCenterY() + STEP);
+        } else if (curretDirection == Direction.LEFT) {
+            snake.setCenterX(snake.getCenterX() - STEP);
+        } else if (curretDirection == Direction.RIGHT) {
+            snake.setCenterX(snake.getCenterX() + STEP);
+        }
+    }
+    private void move(){
+        Platform.runLater(()->{
+            step();
+        });
+    }
+
+ @Override
     public void start(Stage primaryStage){
         root = new Pane();
         root.setPrefSize(WIDTH,HEIGHT);
         random = new Random();
+        curretDirection = Direction.UP;
 
         newFood();
         newSnake();
+
+        Runnable r = ()-> {
+            try{
+                for (;;){
+                    move();
+                    Thread.sleep(200);
+                }
+            } catch (InterruptedException ie){
+
+            }
+        };
+
         Scene scene = new Scene(root, 600, 500);
+        scene.setFill(Color.web("81c483"));
         scene.addEventFilter(KeyEvent.KEY_PRESSED, event-> {
             KeyCode code = event.getCode();
             if (code == KeyCode.UP) {
-                snake.setCenterY(snake.getCenterY() - STEP);
+                curretDirection = Direction.UP;
             } else if (code == KeyCode.DOWN) {
-                snake.setCenterY(snake.getCenterY() + STEP);
+                curretDirection = Direction.DOWN;
             } else if (code == KeyCode.LEFT) {
-                snake.setCenterX(snake.getCenterX() - STEP);
+                curretDirection = Direction.LEFT;
             } else if (code == KeyCode.RIGHT) {
-                snake.setCenterX(snake.getCenterX() + STEP);
+                curretDirection = Direction.RIGHT;
             }
         });
         primaryStage.setTitle("Snake Game");
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
         primaryStage.show();
+        Thread th = new Thread(r);
+        th.setDaemon(true);
+        th.start();
     }
 
 }
+
